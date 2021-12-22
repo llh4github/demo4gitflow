566681c64436bce420bcfbb7255130d4c4b23267
llh <lilinhong_coding@foxmail.com>
2021-12-22 15:54:41 +0800

    MAVEN项目初始化

---
+++ src/test/kotlin/com/github/llh4github/demo4gitflow/Demo4gitflowApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.llh4github.demo4gitflow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Demo4gitflowApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
